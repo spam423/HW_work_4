b7032069ac654925168151c5f4d6d03d0d9cd097
spam423 <zombe423@list.ru>
2023-10-07 02:06:34 +0300

    HM

---
+++ lesson_25/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) и возводит 
+число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+Console.Write("insert number A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("insert number B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int c = 1;
+
+if (b < 1)
+Console.Write("B natural error\n");
+else
+{
+    while(b!=0)
+    {
+        c = c * a;
+        b = b - 1;
+    }
+    Console.WriteLine($"Number А in natural grade B: {c}");
+}
+
---
+++ lesson_29/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 29: Напишите программу, 
+которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
+
+int[] numbers = new int[8];
+Console.Write("[");
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(1, 50);
+    Console.Write("" + Array (i) + ", ");
+}
+Console.Write("]");
+
+int Array (int a)
+{
+    return numbers[a];
+}
